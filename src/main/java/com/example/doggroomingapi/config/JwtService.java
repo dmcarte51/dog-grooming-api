e5208c92e25cce8f7ce20b3283790b0a3d880757
david <dcarte51@msudenver.edu>
2024-02-23 09:49:31 -0700

    add jjwt dependencies to pom

--- src/main/java/com/example/doggroomingapi/config/JwtService.java
+++ src/main/java/com/example/doggroomingapi/config/JwtService.java
@@ -1,4 +1,14 @@
 package com.example.doggroomingapi.config;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
+
+    public String extractUsername(String token) {
+
+        return "";
+    }
+
 }
